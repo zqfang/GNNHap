6601c5435768bb9384293320253984f5066d3bac
zqfang <fangzq@stanford.edu>
2021-11-22 10:09:13 -0800

    bugfix

--- inphernet/train_gnn.py
+++ inphernet/train_gnn.py
@@ -28,37 +28,37 @@ torch.manual_seed(seed=123456)
 
 os.makedirs(args.outdir, exist_ok=True)
 tb = SummaryWriter(log_dir = args.outdir, filename_suffix=".human_genemesh_gnn")
-# # read data in
-print("Load Gene Mesh Graph")
-H = nx.read_gpickle(args.gene_mesh_graph) # note: H is undirected multigraph
+# # # read data in
+# print("Load Gene Mesh Graph")
+# H = nx.read_gpickle(args.gene_mesh_graph) # note: H is undirected multigraph
 
-# Load mesh embeddings
-print("Load embeddings")
-mesh_features = pd.read_csv(args.mesh_embed, index_col=0, header=None)
-gene_features = pd.read_csv(args.gene_embed, index_col=0, header=None)
-gene_features.index = gene_features.index.astype(str)
+# # Load mesh embeddings
+# print("Load embeddings")
+# mesh_features = pd.read_csv(args.mesh_embed, index_col=0, header=None)
+# gene_features = pd.read_csv(args.gene_embed, index_col=0, header=None)
+# gene_features.index = gene_features.index.astype(str)
 
-# print("Build GraphData")
-# # build data
-gm = GeneMeshData(H, gene_features, mesh_features)
-# # align 
-gm_data = gm()
-# save data for future use
-joblib.dump(gm_data, filename=os.path.join(args.outdir,"genemesh.data.pkl"))
-# # train test split
-train_data, val_data, test_data = T.RandomLinkSplit(is_undirected=True, 
-                                         add_negative_train_samples=True, 
-                                         neg_sampling_ratio=1.0,
-                                         edge_types=('gene','genemesh','mesh'), # must be tuple, not list
-                                         rev_edge_types=('mesh','rev_genemesh','gene'))(gm_data)
-print("Save graph data")
-joblib.dump(train_data, filename=os.path.join(args.outdir,"train.data.pkl"))
-joblib.dump(val_data, filename=os.path.join(args.outdir,"val.data.pkl"))
-joblib.dump(test_data, filename=os.path.join(args.outdir,"test.data.pkl"))
+# # print("Build GraphData")
+# # # build data
+# gm = GeneMeshData(H, gene_features, mesh_features)
+# # # align 
+# gm_data = gm()
+# # save data for future use
+# joblib.dump(gm_data, filename=os.path.join(args.outdir,"genemesh.data.pkl"))
+# # # train test split
+# train_data, val_data, test_data = T.RandomLinkSplit(is_undirected=True, 
+#                                          add_negative_train_samples=True, 
+#                                          neg_sampling_ratio=1.0,
+#                                          edge_types=('gene','genemesh','mesh'), # must be tuple, not list
+#                                          rev_edge_types=('mesh','rev_genemesh','gene'))(gm_data)
+# print("Save graph data")
+# joblib.dump(train_data, filename=os.path.join(args.outdir,"train.data.pkl"))
+# joblib.dump(val_data, filename=os.path.join(args.outdir,"val.data.pkl"))
+# joblib.dump(test_data, filename=os.path.join(args.outdir,"test.data.pkl"))
 
 print("Read graph data")
-# train_data = joblib.load(os.path.join(args.outdir,"train.data.pkl"))
-# val_data = joblib.load(os.path.join(args.outdir,"val.data.pkl"))
+train_data = joblib.load(os.path.join(args.outdir,"train.data.pkl"))
+val_data = joblib.load(os.path.join(args.outdir,"val.data.pkl"))
 ##test_datat = joblib.load(os.path.join(args.outdir,"test.data.pkl"))
 ## init model
 hidden_size = args.hidden_size
@@ -71,10 +71,10 @@ optimizer = torch.optim.Adam(model.parameters(), lr=1e-2, weight_decay=1e-4)
 #scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [200, 500, 800], gamma=0.5)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer,T_0=50, T_mult=4, eta_min=1e-4)
 
-def train(train_data: HeteroData, epoch):
+def train(epoch, device='cpu'):
     #train_data.to(device)
     model.train()
-    batch_size=100000
+    batch_size=10000
     edge_label_index = train_data['gene','genemesh','mesh'].edge_label_index
     edge_label = train_data['gene','genemesh','mesh'].edge_label
     # minibatch training for supervision links
@@ -100,13 +100,13 @@ def train(train_data: HeteroData, epoch):
 
 
 @torch.no_grad()
-def valid(data: HeteroData, device='cpu'):
+def valid(epoch, device='cpu'):
     model.eval()
     #data.to(device)
     batch_size=10000
     # supervision links
-    edge_label_index = data['gene','genemesh','mesh'].edge_label_index
-    edge_label = data['gene','genemesh','mesh'].edge_label
+    edge_label_index = val_data['gene','genemesh','mesh'].edge_label_index
+    edge_label = val_data['gene','genemesh','mesh'].edge_label
     # 
     val_loss = 0
     y = []
@@ -116,7 +116,7 @@ def valid(data: HeteroData, device='cpu'):
     valid_auroc = AUROC(pos_label=1, compute_on_step=False)
     valid_acc = Accuracy(threshold=0.5, compute_on_step=False)
 
-    h_dict = model(data.x_dict, data.edge_index_dict) # only need compute once for node_representations
+    h_dict = model(val_data.x_dict, val_data.edge_index_dict) # only need compute once for node_representations
     # minibatch testing for supervision links
     data_loader = DataLoader(torch.arange(edge_label.size(0)), batch_size=batch_size)
     for i, perm in enumerate(tqdm(data_loader, total=len(data_loader), desc='Valid')):
@@ -154,18 +154,9 @@ val_data.to(device)
 model.to(device)
 
 print("Start training")
-for epoch in range(1, 1001):
-    #model.train()
-    #optimizer.zero_grad()
-    #out = model(train_data.x_dict, train_data.edge_index_dict)
-    #loss = model.score_loss(out, train_data.edge_label_index_dict, train_data.edge_label_dict)
-    #loss.backward()
-    #optimizer.step()
-    #scheduler.step()
-    #lr = scheduler.get_last_lr()[0]
-    #train_loss = loss.item()
-    train_loss = train(train_data, epoch)
-    val_metrics = valid(val_data, device)  
+for epoch in range(1, 101):
+    train_loss = train(epoch)
+    val_metrics = valid(epoch)  
     val_loss = val_metrics['val_loss']
     auroc = val_metrics['auroc']
     ap = val_metrics['auroc']
